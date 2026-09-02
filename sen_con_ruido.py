c7ac4209104f7a26f246bed03cd3acdf220ad44b
Casandra Sotelino <csotelino@estudiantes.unsam.edu.ar>
2026-09-02 12:29:27 -0300

    update

--- sen_con_ruido.py
+++ sen_con_ruido.py
@@ -16,12 +16,12 @@ N = 1000
 
 #%%funciones
 
-def function_sen_nq(potx = 1, dc = 0, ff = 1, ph = 0, nn = 1000,
-                    fs = 1000, SNRdB = 10):
+def function_sen_nq(potx = 1, dc_sen = 0, ff = 1, ph = 0, nn = 1000,
+                    fs = 1000, SNRdB = 10, dc_noise = 0):
     tt = np.arange(0, nn/fs, 1/fs) #vector tiempo (segundos)
-    xx = np.pow(potx, 0.5) * np.sin(2 * np.pi * tt * ff * nn / fs + ph) + dc #funcion seno
+    xx = np.pow(potx, 0.5) * np.sin(2 * np.pi * tt * ff * nn / fs + ph) + dc_sen #funcion seno
     des_est = np.sqrt(pow(10, ((-SNRdB/10)+ np.log10(potx))))
-    nq = np.random.normal(0, des_est, nn) #np.random.normal(loc, scale, size) → genera datos normales con media loc y desviación estándar scale.
+    nq = np.random.normal(0, des_est, nn) +dc_noise #np.random.normal(loc, scale, size) → genera datos normales con media loc y desviación estándar scale.
     f_suma = xx + nq
     return(tt, f_suma)
 
@@ -90,7 +90,7 @@ plt.figure(3)
 plt.plot(Pnq)
 i = (N/2)-1
 
-print("varianza: ", np.var(nq), "valor de la energía en el delta: ", Pnq[i])
+#print("varianza: ", np.var(nq), "valor de la energía en el delta: ", Pnq[i])
 
 #copio de chat:
 #%% Test de Kolmogorov-Smirnov
